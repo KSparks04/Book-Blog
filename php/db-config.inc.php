fbf0f22de9e0951232163e0b8781f10e9d59af93
KSparks <kaiyadancer@hotmail.com>
2026-05-21 17:32:02 -0400

    Index change

--- php/db-config.inc.php
+++ php/db-config.inc.php
@@ -1,14 +1,14 @@
 
 <?php
 
-define('DBHOST', 'localhost');
+define('DBHOST', 'tangledblog2026mysqlfix.mysql.database.azure.com');
 define('DBNAME', 'book_review_site');
-define('DBUSER', 'testuser');
-define('DBPASS', 'mypassword');
+define('DBUSER', 'booksadmin');
+define('DBPASS', 'All!blogs!2904');
 //define('DBCONNSTRING','sqlite:./art.db');
 define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
 
-
+//ignore
 
 ?>
 
