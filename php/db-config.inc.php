ffaca86e953ce887512dd8348bc9bea18a32ccbc
KSparks <kaiyadancer@hotmail.com>
2026-05-21 18:19:11 -0400

    Update Config File

--- php/db-config.inc.php
+++ php/db-config.inc.php
@@ -1,14 +1,15 @@
-
 <?php
 
-define('DBHOST', 'tangledblog2026mysqlfix.mysql.database.azure.com');
-define('DBNAME', 'book_review_site');
-define('DBUSER', 'booksadmin');
-define('DBPASS', 'All!blogs!2904');
-//define('DBCONNSTRING','sqlite:./art.db');
-define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;sslmode=require");
+define('DBHOST', getenv('DB_HOST') ?: 'localhost');
+define('DBNAME', getenv('DB_NAME') ?: 'book_review_site');
+define('DBUSER', getenv('DB_USER') ?: 'testuser');
+define('DBPASS', getenv('DB_PASS') ?: 'mypassword');
 
-//ignore
+define(
+    'DBCONNSTRING',
+    "mysql:host=" . DBHOST .
+    ";dbname=" . DBNAME .
+    ";charset=utf8mb4;sslmode=require"
+);
 
 ?>
-
