00b1bbb5c3ecdaaf17e88faaf3d19553f8b60321
KSparks <kaiyadancer@hotmail.com>
2026-05-21 18:06:07 -0400

    Change to index.html again

--- php/db-config.inc.php
+++ php/db-config.inc.php
@@ -6,7 +6,7 @@ define('DBNAME', 'book_review_site');
 define('DBUSER', 'booksadmin');
 define('DBPASS', 'All!blogs!2904');
 //define('DBCONNSTRING','sqlite:./art.db');
-define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
+define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;sslmode=require");
 
 //ignore
 
