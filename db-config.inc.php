de8fc56f2d6d65d25b017877580784651f3d31b2
Kaiya Sparks <kaiyadancer@hotmail.com>
2026-05-21 17:45:00 -0400

    Create db-config.inc.php

---
+++ db-config.inc.php
@@ -0,0 +1,14 @@
+
+<?php
+
+define('DBHOST', 'tangledblog2026mysqlfix.mysql.database.azure.com');
+define('DBNAME', 'book_review_site');
+define('DBUSER', 'booksadmin');
+define('DBPASS', 'All!blogs!2904');
+//define('DBCONNSTRING','sqlite:./art.db');
+define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
+
+//ignore
+
+?>
+
