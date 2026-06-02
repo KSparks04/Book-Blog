9498d13350759925c02925d2260d00e7f540b753
KSparks <kaiyadancer@hotmail.com>
2026-06-02 19:18:03 -0400

    Debug2

--- integrations/oauth2/callback.php
+++ integrations/oauth2/callback.php
@@ -1,11 +1,12 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 
 use Dotenv\Dotenv;
 
-if (file_exists(__DIR__ . '/../.env')) {
-    $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+if (file_exists(__DIR__ . '../../.env')) {
+    $dotenv = Dotenv::createImmutable(__DIR__ . '/../..');
     $dotenv->load();
 }
 session_start();
